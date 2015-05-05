e694a09510566c8dfc06b6346b65d51d0bf967a6
vovd <vovd.tanglu@gmail.com>
2015-05-05 22:11:49 +0200

    Update sweetupload.sh

--- sweetupload.sh
+++ sweetupload.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/ValentinOVD/Open-Word-Database-m2 binary > /dev/null
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/ValentinOVD/Open-Word-Database-bin binary > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd binary
